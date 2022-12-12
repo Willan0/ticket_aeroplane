d2b73fdf3ca70985fb5d0e197db1bd3d167206d5
haru <uchikaharu0125@gmail.com>
2022-12-12 12:01:57 +0630

    ticket_aeroplane

--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class AppLayout{
   static getSize(BuildContext context){
     return MediaQuery.of( context).size;
   }
+  static getWidth()=>Get.width;
+
+  static getHeight()=> Get.height;
+
 }
