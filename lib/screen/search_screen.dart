06eba93bff62f119e55500340ce5c87af801f880
haru <uchikaharu0125@gmail.com>
2022-12-12 19:28:46 +0630

    ticket_aeroplane

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -6,20 +6,19 @@ import 'package:ticket/utils/app_layout.dart';
 import '../utils/app_style.dart';
 
 class Search extends StatelessWidget {
-  const Search({Key? key}) : super(key: key);
 
+  const Search({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Style.bgColor,
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20),vertical: AppLayout.getHeight(20)),
         children: [
           Gap(AppLayout.getHeight(40)),
-          Container(
-            child: Text('What are\n you looking for?',
-              style: Style.headingLineStyle1.copyWith(fontSize: 35,color: Colors.grey.shade600),
-            ),
+          Text('What are\nyou looking for?',
+            style: Style.headingLineStyle1.copyWith(fontSize: 35,color: Colors.grey.shade600),
           ),
           Gap(AppLayout.getHeight(20)),
           Container(
@@ -33,7 +32,7 @@ class Search extends StatelessWidget {
                 Container(
                   height: AppLayout.getHeight(40),
                   alignment: Alignment.center,
-                  child: Text('Air Tickets',style: Style.headingLineStyle4.copyWith(color: Colors.black),),
+                  child: Text('AirLine Tickets',style: Style.headingLineStyle4.copyWith(color: Colors.black),),
                 ),
                 Container(
                   height: AppLayout.getHeight(40),
