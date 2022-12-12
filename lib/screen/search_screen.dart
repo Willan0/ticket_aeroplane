9efbb4369f63801e6022e0b77079f4fe6ad34bf8
haru <uchikaharu0125@gmail.com>
2022-12-12 17:21:15 +0630

    ticket_aeroplane

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -14,8 +14,36 @@ class Search extends StatelessWidget {
       backgroundColor: Style.bgColor,
       body: ListView(
         children: [
-          Gap(AppLayout.getHeight(30)),
-          Text('What are\n you looking for?',style: Style.headingLineStyle1.copyWith(fontSize: 35),)
+          Gap(AppLayout.getHeight(40)),
+          Container(
+            margin: EdgeInsets.only(left: AppLayout.getWidth(20)),
+            child: Text('What are\n you looking for?',
+              style: Style.headingLineStyle1.copyWith(fontSize: 35,color: Colors.grey.shade600),
+            ),
+          ),
+          Gap(AppLayout.getHeight(20)),
+          Container(
+            margin: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(AppLayout.getHeight(20)),
+              color: const Color(0xfff4f6fd)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children:  [
+                Container(
+                  height: AppLayout.getHeight(40),
+                  alignment: Alignment.center,
+                  child: Text('Air Tickets',style: Style.headingLineStyle4.copyWith(color: Colors.black),),
+                ),
+                Container(
+                  height: AppLayout.getHeight(40),
+                  alignment: Alignment.center,
+                  child: Text('Hotels',style: Style.headingLineStyle4.copyWith(color: Colors.black),),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
