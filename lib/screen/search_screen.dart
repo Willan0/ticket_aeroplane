9ffedf190a35f5e062bb78023391ef28002ffc6c
haru <uchikaharu0125@gmail.com>
2022-12-12 23:25:03 +0630

    ticket_aeroplane

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -11,7 +11,7 @@ class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-
+final size = AppLayout.getSize(context);
     return Scaffold(
       backgroundColor: Style.bgColor,
       body: ListView(
@@ -51,9 +51,9 @@ class Search extends StatelessWidget {
           Container(
               height: AppLayout.getHeight(50),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
+                borderRadius: BorderRadius.circular(20),
               ),
-              child: ElevatedButton(onPressed: (){}, child: const Text("Find Tickets"))
+              child: ElevatedButton(onPressed: (){}, child: const Text("Find Tickets",))
           ),
           Gap(AppLayout.getHeight(35)),
           Container(
@@ -65,6 +65,110 @@ class Search extends StatelessWidget {
                 InkWell(onTap: (){},child: Text('View all',style: Style.textStyle,))
               ],
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: size.width*0.41,
+                height: AppLayout.getHeight(440),
+                padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(10)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      spreadRadius: 1,
+                      blurRadius: 1
+                    )
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(250),
+                      decoration:  BoxDecoration(
+                        borderRadius: BorderRadius.circular(AppLayout.getHeight(12)),
+                        image: const DecorationImage(fit:BoxFit.cover,image: AssetImage('assets/images/sit.jpg'))
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(10)),
+                    Text("20% discount on the early booking of this flight, Don't miss out this chance ",style: Style.headingLineStyle3.copyWith(color: Colors.black, ),)
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Stack(
+                    alignment: AlignmentDirectional.topEnd,
+                    children: [
+                      Container(
+                        width: size.width*0.45,
+                        height: AppLayout.getHeight(215),
+                        decoration: BoxDecoration(
+                          color: const Color(0xff3ab8b8),
+                          borderRadius: BorderRadius.circular(AppLayout.getHeight(12)),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(15),vertical: AppLayout.getHeight(15)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Discount\nfor survey',style: Style.headingLineStyle2.copyWith(fontWeight: FontWeight.bold,color: Colors.white),),
+                            Text('Take the survey about our services and get Discount',style: Style.headingLineStyle4.copyWith(color: Colors.white,fontSize: 18),),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -50,
+                        top: -50,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(width:18,color: const Color(0xff189999))
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Gap(AppLayout.getHeight(15)),
+                  Container(
+                    width: size.width*0.45,
+                    height: AppLayout.getHeight(210),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffec6545),
+                      borderRadius: BorderRadius.circular(AppLayout.getHeight(12)),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(15),vertical: AppLayout.getHeight(15)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(textAlign: TextAlign.center,'Take Love',style: Style.headingLineStyle2.copyWith(fontWeight: FontWeight.bold,color: Colors.white),),
+                        Gap(AppLayout.getHeight(10)),
+                        RichText(
+                            text: const TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '😘',
+                                  style: TextStyle(fontSize: 35)
+                                ),
+                                TextSpan(
+                                    text: '😍',
+                                    style: TextStyle(fontSize: 35)
+                                ),
+                                TextSpan(
+                                    text: '🥰',
+                                    style: TextStyle(fontSize: 35)
+                                ),
+                              ]
+                        ))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
